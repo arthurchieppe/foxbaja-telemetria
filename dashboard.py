910ffd822e7957203b3c938f3e0870384839db9c
arthurchieppe <agchieppe@gmail.com>
2022-08-23 12:45:14 -0300

    Add labels vel, temp, comb

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pandas
 import numpy as np 
 
+velocidade=59
+temp_cvt=34
+combustivel=1
+
 # def get_data():
     # pd.read_csv()
     # or
@@ -9,9 +13,35 @@ import numpy as np
     # return data
 
 st.set_page_config(
-    page_title="Dashboard Fox Baja"
-    page_icon="🦊"
+    page_title="Dashboard Fox Baja",
+    page_icon="🦊",
     layout="wide"
 )
 
 st.title('Dashboard Fox Baja')
+st.columns(3)
+
+kpi1, kpi2, kpi3 = st.columns(3)
+
+# Velocidade:
+kpi1.metric(
+    label="Velocidade",
+    value=int(velocidade)
+)
+
+# Temperatura:
+kpi2.metric(
+    label="Temperatura da CVT",
+    value=int(temp_cvt)
+)
+
+# Gasolina:
+kpi3.metric(
+    label="Nivel de combustivel",
+    value=combustivel
+)
+
+
+
+fig_vel, fig_temp = st.columns(2)
+
