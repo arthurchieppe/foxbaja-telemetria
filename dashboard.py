2220267ec67f08b7e0fada379386cd57b820c0c4
arthurchieppe <agchieppe@gmail.com>
2022-08-23 11:21:16 -0300

    Add pipfile and main .py

---
+++ dashboard.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pandas
+import numpy as np 
+
+# def get_data():
+    # pd.read_csv()
+    # or
+    # read JSON
+    # return data
+
+st.set_page_config(
+    page_title="Dashboard Fox Baja"
+    page_icon="🦊"
+    layout="wide"
+)
+
+st.title('Dashboard Fox Baja')
