7b45d2f735393fe9df44e1527a83ad3dd782c3d5
arthurchieppe <agchieppe@gmail.com>
2022-08-29 20:11:49 -0300

    Progresso Dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,16 +1,30 @@
 import streamlit as st
-import pandas as pandas
+import pandas as pd
 import numpy as np 
+import plotly.express as px
+import time
+import random
+import datetime
 
 velocidade=59
 temp_cvt=34
 combustivel=1
 
-# def get_data():
-    # pd.read_csv()
-    # or
-    # read JSON
-    # return data
+def get_data():
+    ls_time = []
+    now = datetime.datetime.now()
+    for i in range(0,20):
+        now -= datetime.timedelta(seconds=1)
+        ls_time.append(now)
+        
+    data = {
+        'velocidade': random.sample(range(0,40), 20),
+        'temperatura': random.sample(range(60,80), 20),
+        'tempo' : ls_time
+        }
+    df = pd.DataFrame(data=data)
+    return df
+df = get_data()
 
 st.set_page_config(
     page_title="Dashboard Fox Baja",
@@ -45,3 +59,24 @@ kpi3.metric(
 
 fig_vel, fig_temp = st.columns(2)
 
+with fig_vel:
+    st.markdown("### Histórico de Velocidade")
+    fig = px.line(df, x="tempo", y="velocidade")
+    fig.update_layout(
+    autosize=False,
+    width=600,
+    height=500)
+    st.write(fig)
+
+with fig_temp:
+    st.markdown('### Histórico de temperatura')
+    fig2 = fig = px.line(df, x="tempo", y="temperatura")
+    fig2.update_layout(
+    autosize=False,
+    width=600,
+    height=500)
+    st.write(fig2)
+
+
+st.markdown("### Base completa")
+st.dataframe(df)
